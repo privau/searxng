7494a64ce645a9c02fca687fd5bd10ad64c2d03b
Brock Vojković <brockv@tuta.io>
2022-06-29 08:13:37 +0000

    Removed Donate & About Buttons

--- update.sh
+++ update.sh
@@ -14,12 +14,13 @@ else
 fi
 
 echo "delete upstream simple theme definitions"
-rm -f build/searx/static/themes/simple/src/less/definitions.less build/searx/static/themes/simple/src/less/search.less build/searx/static/themes/simple/src/less/autocomplete.less
+rm -f build/searx/static/themes/simple/src/less/definitions.less build/searx/static/themes/simple/src/less/search.less build/searx/static/themes/simple/src/less/autocomplete.less build/searx/static/themes/simple/src/less/style.less
 
 echo "copy fork simple theme definitions in place"
 cp -v src/less/definitions.less build/searx/static/themes/simple/src/less/definitions.less
 cp -v src/less/autocomplete.less build/searx/static/themes/simple/src/less/autocomplete.less
 cp -v src/less/search.less build/searx/static/themes/simple/src/less/search.less
+cp -v src/less/style.less build/searx/static/themes/simple/src/less/style.less
 
 echo "build themes with upstream scripts"
 cd build
