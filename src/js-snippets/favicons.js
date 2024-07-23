c252cc3ba96cb5b458056e633f15bcae8b243b97
Brock Vojkovic <brockv@tuta.io>
2024-07-23 10:42:52 +0000

    [mod] update favicon size and style

--- src/js-snippets/favicons.js
+++ src/js-snippets/favicons.js
@@ -9,42 +9,41 @@ searxng.ready(function () {
     return;
   }
 
-  const defaultFavicon = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsSAAALEgHS3X78AAACiElEQVQ4EaVTzU8TURCf2tJuS7tQtlRb6UKBIkQwkRRSEzkQgyEc6lkOKgcOph78Y+CgjXjDs2i44FXY9AMTlQRUELZapVlouy3d7kKtb0Zr0MSLTvL2zb75eL838xtTvV6H/xELBptMJojeXLCXyobnyog4YhzXYvmCFi6qVSfaeRdXdrfaU1areV5KykmX06rcvzumjY/1ggkR3Jh+bNf1mr8v1D5bLuvR3qDgFbvbBJYIrE1mCIoCrKxsHuzK+Rzvsi29+6DEbTZz9unijEYI8ObBgXOzlcrx9OAlXyDYKUCzwwrDQx1wVDGg089Dt+gR3mxmhcUnaWeoxwMbm/vzDFzmDEKMMNhquRqduT1KwXiGt0vre6iSeAUHNDE0d26NBtAXY9BACQyjFusKuL2Ry+IPb/Y9ZglwuVscdHaknUChqLF/O4jn3V5dP4mhgRJgwSYm+gV0Oi3XrvYB30yvhGa7BS70eGFHPoTJyQHhMK+F0ZesRVVznvXw5Ixv7/C10moEo6OZXbWvlFAF9FVZDOqEABUMRIkMd8GnLwVWg9/RkJF9sA4oDfYQAuzzjqzwvnaRUFxn/X2ZlmGLXAE7AL52B4xHgqAUqrC1nSNuoJkQtLkdqReszz/9aRvq90NOKdOS1nch8TpL555WDp49f3uAMXhACRjD5j4ykuCtf5PP7Fm1b0DIsl/VHGezzP1KwOiZQobFF9YyjSRYQETRENSlVzI8iK9mWlzckpSSCQHVALmN9Az1euDho9Xo8vKGd2rqooA8yBcrwHgCqYR0kMkWci08t/R+W4ljDCanWTg9TJGwGNaNk3vYZ7VUdeKsYJGFNkfSzjXNrSX20s4/h6kB81/271ghG17l+rPTAAAAAElFTkSuQmCC'
+  const defaultFavicon = 'data:image/svg+xml;utf8,<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style="fill: rgb(64, 73, 81);"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/></svg>';
+  const currentDomain = window.location.host;
+
+  function createFaviconElement (host) {
+    const faviconDiv = document.createElement('div');
+    faviconDiv.className = 'favicon';
+
+    const icon = document.createElement('img');
+    icon.alt = host;
+    icon.src = `https://${currentDomain}/favicon/${host}`;
+    icon.onerror = () => {
+      icon.src = defaultFavicon;
+    };
+
+    faviconDiv.appendChild(icon);
+    return faviconDiv;
+  }
 
   function addFavicons () {
     document.querySelectorAll('.result > .url_wrapper, .result > .result_header').forEach(el => {
-      if (el.querySelector('.favicon')) {
-        return;
+      if (!el.querySelector('.favicon')) {
+        const link = el.href || el.querySelector('a').href;
+        const host = new URL(link).host;
+        const faviconElement = createFaviconElement(host);
+        el.prepend(faviconElement);
       }
-
-      const link = el.href || el.querySelector('a').href;
-      const host = new URL(link).host;
-      const icon = document.createElement('img');
-      const currentDomain = window.location.hostname;
-
-      icon.className = 'favicon';
-      icon.alt = host;
-      icon.src = `https://${currentDomain}/favicon/${host}`;
-      icon.onerror = () => {
-        icon.src = defaultFavicon;
-      };
-
-      el.prepend(icon);
     });
   }
 
-  // Initial call to add favicons to existing elements
-  addFavicons();
-
-  // Set up a MutationObserver to watch for new elements
   const observer = new MutationObserver(mutations => {
     mutations.forEach(mutation => {
       if (mutation.type === 'childList') {
         mutation.addedNodes.forEach(node => {
           if (node.nodeType === 1) { // Check if node is an element
-            if (node.matches('.result > .url_wrapper, .result > .result_header')) {
-              addFavicons();
-            } else if (node.querySelector('.result > .url_wrapper, .result > .result_header')) {
+            if (node.matches('.result > .url_wrapper, .result > .result_header') || node.querySelector('.result > .url_wrapper, .result > .result_header')) {
               addFavicons();
             }
           }
@@ -53,9 +52,10 @@ searxng.ready(function () {
     });
   });
 
-  // Start observing the results container for changes
   const resultsContainer = document.querySelector('#results');
   if (resultsContainer) {
     observer.observe(resultsContainer, { childList: true, subtree: true });
   }
+
+  addFavicons();
 });
